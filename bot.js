8a90b2cb42653db5a102cb8a11f1bf5d612bfb39
nicolayjr <nicolay.olivier@hotmail.com>
2019-05-30 01:47:31 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+cosnt Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping');
+    }
+});
+
+client.login(process.env.NTgxMDgxMDAxODI1MDA5Njc4.XO8Z-A.vpq7VSxUYinmO3sckinr5hFS3ck)
+    
