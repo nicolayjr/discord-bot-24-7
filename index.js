c9d78c71a50888aada1ec3f6754c20657022c147
nicolayjr <nicolay.olivier@hotmail.com>
2019-05-30 00:49:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Discord = require("discord.js");
-
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online`)
-  bot.user.setActivity("Do (.help) for the bot to take action");
-  bot.user.setStatus('online')
-})
+bot.on('ready', () => {
+  console.log('ready')
+});
+
+bot.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
 
 bot.login(process.env.NTY2MjU1NjA0ODY4NDQ4Mjc2.XLCigQ.A8jxYbAcmwrjHLIFpNlBLzzLnAA);
