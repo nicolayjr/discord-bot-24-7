553102ed761884113d9eabf088903cc69288459a
nicolayjr <nicolay.olivier@hotmail.com>
2019-05-30 01:08:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
-
-bot.on('ready', () => {
-  console.log('ready')
-});
+const index = require("index.js");  
+const client = new Discord.client()
 
 bot.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
-  }
-});
-
-bot.login(process.env.NTY2MjU1NjA0ODY4NDQ4Mjc2.XLCigQ.A8jxYbAcmwrjHLIFpNlBLzzLnAA);
+    if (message.content === 'ping') {
+      message.reply('pong');
+    }
+  });
+  
+  bot.login(process.env.NTY2MjU1NjA0ODY4NDQ4Mjc2.XLCigQ.A8jxYbAcmwrjHLIFpNlBLzzLnAA);A);
