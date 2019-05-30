d9a5a8e9c8c37a6866c93843b0a68a400ab6f503
nicolayjr <nicolay.olivier@hotmail.com>
2019-05-30 02:13:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-cosnt Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -6,9 +6,11 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping');
-    }
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-client.login(process.env.BOT_TOKEN)
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
     
